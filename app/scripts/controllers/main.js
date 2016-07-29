fb6829d2055815ce33b9a2de68c55efa7c14d457
Puhi <inmpuhi@ad.harman.com>
2016-07-29 17:46:20 +0530

    --Angular Social App first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSocialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSocialApp
+ */
+angular.module('angularSocialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
